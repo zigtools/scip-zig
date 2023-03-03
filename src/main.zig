6d9cb6913ba6ea7458edad52d69180e5097cc4cb
Auguste Rame <19855629+SuperAuguste@users.noreply.github.com>
2023-03-03 02:02:29 -0500

    use zls as a library

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,8 @@
 const std = @import("std");
+const zls = @import("zls");
 const scip = @import("scip.zig");
 const protobruh = @import("protobruh.zig");
-const StoreToScip = @import("StoreToScip.zig");
-const DocumentStore = @import("analysis/DocumentStore.zig");
-const utils = @import("analysis/utils.zig");
+// const StoreToScip = @import("StoreToScip.zig");
 
 const ArgState = enum {
     none,
@@ -17,96 +16,31 @@ pub fn main() !void {
     // TODO: Use GPA once memory improves; see issue #1
     const allocator = std.heap.page_allocator;
 
-    var doc_store = DocumentStore{
-        .allocator = allocator,
-        .root_path = "",
-    };
+    var config = zls.Config{};
+    var version: ?zls.ZigVersionWrapper = null;
 
-    var cwd_buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+    try zls.configuration.configChanged(&config, &version, allocator, null);
 
-    var root_path: []const u8 = try std.os.getcwd(&cwd_buf);
-    var root_name: ?[]const u8 = null;
-    var package_name: ?[]const u8 = null;
+    var server = try zls.Server.create(allocator, &config, null, false, false);
+    defer server.destroy();
 
-    var arg_state: ArgState = .none;
-    var arg_iterator = try std.process.ArgIterator.initWithAllocator(allocator);
-    defer arg_iterator.deinit();
+    const handle = try server.document_store.createDocumentFromPath(try allocator.dupe(u8, "C:/Programming/Zig/scip-zig/src/main.zig"), true);
+    const scopes = handle.?.document_scope.scopes;
 
-    doc_store.root_path = root_path;
+    const locs = scopes.items(.loc);
+    const decls_list = scopes.items(.decls);
 
-    while (arg_iterator.next()) |arg| {
-        switch (arg_state) {
-            .none => {
-                if (std.mem.eql(u8, arg, "--pkg")) arg_state = .add_package_name;
-                if (std.mem.eql(u8, arg, "--root-pkg")) arg_state = .root_name;
-                if (std.mem.eql(u8, arg, "--root-path")) arg_state = .root_path;
-            },
-            .add_package_name => {
-                package_name = arg;
-                arg_state = .add_package_path;
-            },
-            .add_package_path => {
-                try doc_store.createPackage(package_name.?, arg);
-                arg_state = .none;
-            },
-            .root_name => {
-                if (root_name != null) std.log.err("Multiple roots detected; this invocation may not behave as expected!", .{});
-                root_name = arg;
-                arg_state = .none;
-            },
-            .root_path => {
-                if (root_name != null) std.log.err("Multiple roots detected; this invocation may not behave as expected!", .{});
-                root_path = arg;
-                doc_store.root_path = root_path;
-                arg_state = .none;
-            },
+    for (locs, decls_list) |loc, decls| {
+        var it = decls.iterator();
+        while (it.next()) |decl| {
+            switch (decl.value_ptr.*) {
+                .ast_node => |bruh| {
+                    std.log.info("scope {any} | decl {s} | {s}", .{ loc, decl.key_ptr.*, handle.?.tree.getNodeSource(bruh) });
+                },
+                else => {
+                    std.log.info("scope {any} | decl {s} | {any}", .{ loc, decl.key_ptr.*, decl });
+                },
+            }
         }
     }
-
-    if (root_name == null) {
-        std.log.err("Please specify a root package name with --root-pkg!", .{});
-        return;
-    }
-
-    var it = (doc_store.packages.get(root_name.?) orelse {
-        std.log.err("Root package not found!", .{});
-        return;
-    }).handles.iterator();
-    while (it.next()) |i| {
-        try i.value_ptr.*.analyzer.postResolves();
-    }
-
-    var index = try std.fs.cwd().createFile("index.scip", .{});
-    defer index.close();
-
-    var documents = try StoreToScip.storeToScip(allocator, &doc_store, root_name.?);
-
-    var arg_reiterator = try std.process.ArgIterator.initWithAllocator(allocator);
-    defer arg_reiterator.deinit();
-
-    var bufw = std.io.bufferedWriter(index.writer());
-
-    const project_root = try utils.fromPath(allocator, root_path);
-    std.log.info("Using project root {s}", .{project_root});
-
-    try protobruh.encode(scip.Index{
-        .metadata = .{
-            .version = .unspecified_protocol_version,
-            .tool_info = .{
-                .name = "scip-zig",
-                .version = "unversioned",
-                .arguments = args: {
-                    var arguments = std.ArrayListUnmanaged([]const u8){};
-                    while (arg_reiterator.next()) |arg| try arguments.append(allocator, arg);
-                    break :args arguments;
-                },
-            },
-            .project_root = project_root,
-            .text_document_encoding = .utf8,
-        },
-        .documents = documents,
-        .external_symbols = .{},
-    }, bufw.writer());
-
-    try bufw.flush();
 }
